07ae12e734c43b950f049a8c36a04fb56ff6bc34
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-03-05 11:41:37 +0000

    `Added GestureHandlerRootView and Slider component to OnboardingScreen`

--- screen/Onboarding/OnboardingScreen.tsx
+++ screen/Onboarding/OnboardingScreen.tsx
@@ -1,15 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
+import { GestureHandlerRootView} from "react-native-gesture-handler"
 import { onBoardingSlides } from '@/config/constants'
+import Slider from '@/components/onbarding/slider'
 
 const OnboardingScreen = () => {
     const [index, setIndex] = useState(0)
     const prev = onBoardingSlides[index-1]
     const next = onBoardingSlides[index+1]
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+        <Slider 
+        
+        />
+    </GestureHandlerRootView>
   )
 }
 
