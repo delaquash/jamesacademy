e17f9e61350b7fda0e2d870c74a1227b995c7450
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-03-12 13:06:47 +0000

    feat: implement new layout component and update authentication token retrieval logic

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 import React, { useState, useEffect } from 'react';
-// import {  } from 'react';
 import * as SecureStore from 'expo-secure-store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Redirect } from 'expo-router';
@@ -10,14 +9,16 @@ export default function index() {
     const [loading, setLoading] = useState(true);
 
 
-    useEffect(async()=> {
-        const subscription= async() => {
-            const token = await SecureStore.getItemAsync('token');
-            setLoggedInUser(token ? true : false )
-            setLoading(false)
-        }
-        subscription()
-    },[])
+    useEffect(() => {
+        const subscription = async () => {
+            const token = Platform.OS === "web"
+            ? await AsyncStorage.getItem("accessToken")
+            : await SecureStore.getItemAsync("accessToken");
+            setLoggedInUser(token ? true : false);
+            setLoading(false);
+        };
+        subscription();
+    }, []);
   return (
    <>
         {loading ? (
