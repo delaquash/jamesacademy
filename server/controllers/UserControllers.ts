7450e6feb0f3c1cf52d1d8f13557caa457d17d70
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-03-15 16:06:28 +0000

    feat: add UserControllers for user login and import necessary dependencies

---
+++ server/controllers/UserControllers.ts
@@ -0,0 +1,11 @@
+require("dotenv").config();
+import { Request, Response, NextFunction } from "express";
+
+
+export const UserLogin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
