6f616d6ec58b36fa9cbb9d3657181de6ccd73df2
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-03-15 16:51:37 +0000

    feat: set up Express server and initialize routes for user API

---
+++ server/app.ts
@@ -0,0 +1,50 @@
+require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
+
+import userRoutes from "./route/UserRoute";
+// import { errorHandler } from "./middleware/errorHandler";
+import { CustomError } from "./utils/CustomError";
+
+
+export const app = express();
+const server = require("http").createServer(app);
+
+app.use(express.json({ limit: "100mb" }));
+
+// routes
+app.use("/api/v1/user", userRoutes);
+// app.use("/api/v1/driver", driverRoutes);
+
+// testing route
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is working!",
+    });
+});
+
+
+const errorHandler = (
+    error: CustomError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    console.error('Error:', error);
+    
+    const status = error.statusCode || 500;
+    const message = error.message || 'Internal Server Error';
+    
+    return res.status(status).json({
+        success: false,
+        message
+    });
+};
+
+// app.use(errorHandler)
+
+
+// start server
+server.listen(process.env.PORT, () =>{ 
+    console.log(`Server is running on port ${process.env.PORT}`)
+});
--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,10 @@
-require("dotenv").config();
-import express, { Request, Response, NextFunction } from "express";
-export const app = express();
+require('dotenv').config();
+import http from "http";
+import { app } from "./app";
+
+const server = http.createServer(app);
+
+// start server
+server.listen(process.env.PORT, () =>{ 
+    console.log(`Server is running on port ${process.env.PORT}`)
+});
