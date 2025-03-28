ecbc42d44af839e7174fff2fb9edc1d4b7da1228
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-03-28 19:56:45 +0000

    feat: enhance WelcomeHeader with user greeting, notifications, and linear gradient background

--- components/home/WelcomeHeader.tsx
+++ components/home/WelcomeHeader.tsx
@@ -1,23 +1,140 @@
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, Pressable, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { useTheme } from '@/context/ThemeContext'
 import { useUserData } from '@/hooks/fetch/userData'
+import { LinearGradient } from 'expo-linear-gradient'
+import { fontSizes, IsAndroid, IsHaveNotch, IsIPAD, windowHeight, windowWidth } from '@/themes/app.constant'
+import { moderateScale, scale, verticalScale } from 'react-native-size-matters'
+import { Ionicons } from '@expo/vector-icons'
 
 
 const WelcomeHeader = () => {
     const {theme} = useTheme()
     const {data,isLoading, isError, error} = useUserData();
+    const [notificationLength, setnotificationLength] = useState(0);
 
     if (isLoading) return <ActivityIndicator />;
     if (error) return <Text>Error loading user data</Text>;
 
   return (
-    <View>
-      <Text>WelcomeHeader</Text>
-    </View>
+    <LinearGradient
+        colors={theme.dark ? ["#3c43485c", "#3c43485c", "#3c43485c"]: ["#75ABFC", "#0047AB"]}
+        start={theme.dark ? {x: 1, y: 1} : {x:1, y: 1}}
+        end={theme.dark ? {x:0, y: 1} : {x:0, y: 1}}
+        style={styles.headerWrapper}        
+    >
+        <StatusBar barStyle={"light-content"} />
+        <View
+            style={{
+                flexDirection: 'row',
+                paddingTop: IsHaveNotch ? 
+                IsIPAD 
+                    ? verticalScale(30) 
+                    : verticalScale(40)
+                : verticalScale(30),
+                justifyContent: 'space-between'
+            }}
+        >
+            <View>
+                <Text
+                    style={{
+                        fontSize: fontSizes.FONT32,
+                        color: "#fff",
+                        fontFamily: "Poppins_600SemiBold",
+                    }}
+                >
+                    Hi {data?.name.split(" ")[0]},
+                </Text>
+                <Text
+                    style={{
+                        fontSize: fontSizes.FONT22,
+                        color: "#fff",
+                        fontFamily: "Poppins_400Regular"
+                    }}
+                >
+                    Let's start learning
+                </Text>
+            </View>
+            <View
+                style={{
+                    flexDirection: "row"
+                }}            
+            >
+                <Pressable>
+                    <View
+                        style={[styles.notificationWrapper, {
+                            backgroundColor: theme.dark ? "transparent": "#004FAB",
+                            borderWidth: theme.dark ? 1 : 0,
+                            borderColor: theme.dark ? "#fff" : "transparent",
+                        }]}
+                    >
+                        <Ionicons 
+                            name='notifications-sharp'
+                            size={scale(25)}
+                            color={"#fff"}
+                        />
+                        <View
+                            style={[styles.dot, {
+                                position: "absolute",
+                                right: windowWidth(15),
+                                top: windowHeight(10)
+                            }]}
+                        >
+                            <Text style={{ fontSize: fontSizes.FONT14, color: "#fff"}}>
+                                {notificationLength}
+                            </Text>
+                        </View>
+                    </View>
+                </Pressable>
+            </View>
+        </View>
+    </LinearGradient>
   )
 }
 
 export default WelcomeHeader
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    headerWrapper: {
+        /* This code snippet is setting the height of the `headerWrapper` style based on certain
+        conditions. */
+        height: IsHaveNotch 
+        ?  IsIPAD 
+            ? verticalScale(175) 
+            : verticalScale(155)
+        : IsAndroid 
+            ? verticalScale(168)
+            : verticalScale(152),
+        paddingHorizontal:moderateScale(25),
+        borderBottomLeftRadius: moderateScale(40),
+        borderBottomRightRadius: moderateScale(40),
+        paddingTop: IsAndroid ? verticalScale(10) : verticalScale(20)
+    },
+    notificationWrapper: {
+        position: "relative",
+        width: scale(45),
+        height: scale(45),
+        borderRadius: scale(10),
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    dot: {
+        width: scale(13),
+        height: scale(13),
+        backgroundColor: "#19C964",
+        borderRadius: scale(100),
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    input: {
+        height: IsHaveNotch ? verticalScale(35) : verticalScale(40),
+        backgroundColor: "#fff",
+        color: "#000",
+        marginTop: verticalScale(12),
+        fontSize: IsIPAD ? fontSizes.FONT15 : fontSizes.FONT15,
+        borderRadius: moderateScale(30),
+        paddingHorizontal: moderateScale(15),
+        fontFamily: "Poppins_400Regular"
+    }
+
+})
