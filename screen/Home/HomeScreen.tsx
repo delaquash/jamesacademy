e09fc5110236eda2f75ed89949b8f59abc061d41
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-03-28 16:07:51 +0000

    feat: implement linear gradient background in HomeScreen with theme support

--- screen/Home/HomeScreen.tsx
+++ screen/Home/HomeScreen.tsx
@@ -1,11 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+import { useTheme } from '@/context/ThemeContext'
+
 
 const HomeScreen = () => {
+  const { theme } = useTheme()
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <LinearGradient
+      colors={theme.dark ? ['#180D41', "#2A2D32", "#131313"] : ["#fff", "#f7f7f7"]}
+      start={{ x: 0, y:0}}
+      end={{ x: 0, y:1 }}
+      style={{
+        flex: 1,
+        backgroundColor: theme.dark ? "#101010" : "#fff"
+      }}
+    >
+      <WelcomeHeader />
+    </LinearGradient>
   )
 }
 
