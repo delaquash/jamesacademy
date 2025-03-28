f1141903c1bbfdbbb99e0b48bb06678ffee80d67
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-03-28 16:44:55 +0000

    feat: add loading and error handling for user data in WelcomeHeader component

--- components/home/WelcomeHeader.tsx
+++ components/home/WelcomeHeader.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useTheme } from '@/context/ThemeContext'
+import { useUserData } from '@/hooks/fetch/userData'
 
 
 const WelcomeHeader = () => {
+    const {theme} = useTheme()
+    const {data,isLoading, isError, error} = useUserData();
+
+    if (isLoading) return <ActivityIndicator />;
+    if (error) return <Text>Error loading user data</Text>;
+
   return (
     <View>
       <Text>WelcomeHeader</Text>
