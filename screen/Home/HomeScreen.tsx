eb31020e68c1be709a1bcce64ae31be6afc663e5
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-03 16:10:35 +0000

    feat: add loading state and project description to HomeScreen for improved user experience

--- screen/Home/HomeScreen.tsx
+++ screen/Home/HomeScreen.tsx
@@ -1,17 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useTheme } from '@/context/ThemeContext'
 import WelcomeHeader from '@/components/home/WelcomeHeader'
 import { ScrollView } from 'react-native-gesture-handler'
 import HomeBanner from '@/components/home/HomeBanner'
 import { fontSizes, windowHeight, windowWidth } from '@/themes/app.constant'
-import { verticalScale } from 'react-native-size-matters'
+import { scale, verticalScale } from 'react-native-size-matters'
 import GradientText from '@/components/common/GradientText'
 
 
 const HomeScreen = () => {
   const { theme } = useTheme()
+  const [ loading, setLoading ] = useState(true)
   return (
     <LinearGradient
       colors={theme.dark ? ['#180D41', "#2A2D32", "#131313"] : ["#fff", "#f7f7f7"]}
@@ -52,11 +53,32 @@ const HomeScreen = () => {
               text="Courses"
               styles={{
                 fontSize: fontSizes.FONT35,
-                fontFamily: "Poppins_500Medium"
+                fontFamily: "Poppins_500Medium",
+                paddingLeft: scale(5)
               }}
             />
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View
+                  style={{
+                    backgroundColor: "#12BB70",
+                    width: windowWidth(15),
+                    height: windowWidth(15),
+                    borderRadius: 100,
+                  }}
+                />
+                    <Text
+                      style={{
+                        fontFamily: "Poppins_400Regular",
+                        fontSize: fontSizes.FONT18,
+                        paddingLeft: windowWidth(5),
+                        color: theme.dark ? "#fff" : "#000",
+                      }}
+                    >
+                        Our comprehensive project based courses
+                    </Text>
+                </View>
+              </View>
           </View>
-        </View>
       </ScrollView>
     </LinearGradient>
   )
