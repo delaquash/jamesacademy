d52b44362713c5a2bd8173fa84d8bb639a022ed4
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-07 12:06:23 +0100

    feat: add NotificationScreen component and its index route

---
+++ app/(routes)/notification/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import NotificationScreen from '@/screen/notification/NotificationScreen'
+
+const index = () => {
+  return (
+    <NotificationScreen />
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
