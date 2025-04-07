2465e6d2d84d60c09799364416198ad6ff99b7a4
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-07 11:51:54 +0100

    feat: add handlePress function to SourceCodeCard for opening URLs

--- components/card/SourceCodeCard.tsx
+++ components/card/SourceCodeCard.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import * as WebBrowser from "expo-web-browser";
+import { useTheme } from '@/context/ThemeContext';
 interface Props {
     item: {
         url: string;
@@ -8,6 +10,14 @@ interface Props {
     }
 }
 const SourceCodeCard = ({item } : Props) => {
+    const { theme } = useTheme()
+    /**
+     * The handlePress function uses WebBrowser to open the URL specified in the item object
+     * asynchronously.
+     */
+    const handlePress = async() => {
+        await WebBrowser.openBrowserAsync(item.url)
+    }
   return (
     <View>
       <Text>SourceCodeCard</Text>
@@ -17,4 +27,6 @@ const SourceCodeCard = ({item } : Props) => {
 
 export default SourceCodeCard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
