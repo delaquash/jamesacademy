9c086354fee12ed3c9ddb60eedd26479204b849e
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-10 15:30:23 +0100

    feat: add initial SupportCenter component with basic layout

---
+++ screen/SupportCenter/SupportCenter.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SupportCenter = () => {
+  return (
+    <View>
+      <Text>SupportCenter</Text>
+    </View>
+  )
+}
+
+export default SupportCenter
+
+const styles = StyleSheet.create({})
