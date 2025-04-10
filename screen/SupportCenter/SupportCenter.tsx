99cb12dda51f245eef237e67101a207d040b0d93
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-10 15:31:44 +0100

    feat: expand SupportCenter component with additional imports for enhanced functionality

--- screen/SupportCenter/SupportCenter.tsx
+++ screen/SupportCenter/SupportCenter.tsx
@@ -1,6 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {
+    ActivityIndicator,
+    Linking,
+    Modal,
+    Pressable,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+  } from "react-native";
+  import React, { useState } from "react";
+  import { useTheme } from "@/context/theme.context";
+  import { scale, verticalScale } from "react-native-size-matters";
+  import { router } from "expo-router";
+  import { AntDesign } from "@expo/vector-icons";
+  import SupportBannerOne from "@/assets/svgs/support-center/one";
+  import SupportBannerTwo from "@/assets/svgs/support-center/two";
+  import SupportBannerThree from "@/assets/svgs/support-center/three";
+  import { fontSizes, IsHaveNotch, IsIPAD } from "@/themes/app.constant";
+  import IconOne from "@/assets/svgs/onboarding/icon-1";
+  import IconTwo from "@/assets/svgs/onboarding/icon-2";
+  import IconThree from "@/assets/svgs/onboarding/icon-3";
+  import { BlurView } from "expo-blur";
+  import useUser, { setAuthorizationHeader } from "@/hooks/fetch/useUser";
+  import axios from "axios";
 const SupportCenter = () => {
   return (
     <View>
