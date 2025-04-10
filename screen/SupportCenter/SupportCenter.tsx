380f46ae9a5cabda851328984b400f8d93e480d8
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-10 16:38:25 +0100

    fix: correct import paths for SVGs and update fetchUser hook import

--- screen/SupportCenter/SupportCenter.tsx
+++ screen/SupportCenter/SupportCenter.tsx
@@ -15,15 +15,15 @@ import {
   import { scale, verticalScale } from "react-native-size-matters";
   import { router } from "expo-router";
   import { AntDesign } from "@expo/vector-icons";
-  import SupportBannerOne from "@/assets/svgs/support-center/one";
-  import SupportBannerTwo from "@/assets/svgs/support-center/two";
-  import SupportBannerThree from "@/assets/svgs/support-center/three";
+  import SupportBannerOne from "@/assets/svg/support-center/one";
+  import SupportBannerTwo from "@/assets/svg/support-center/two";
+  import SupportBannerThree from "@/assets/svg/support-center/three";
   import { fontSizes, IsHaveNotch, IsIPAD } from "@/themes/app.constant";
-  import IconOne from "@/assets/svgs/onboarding/icon-1";
-  import IconTwo from "@/assets/svgs/onboarding/icon-2";
-  import IconThree from "@/assets/svgs/onboarding/icon-3";
+  import IconOne from "@/assets/svg/onboarding/icon-1";
+  import IconTwo from "@/assets/svg/onboarding/icon-2";
+  import IconThree from "@/assets/svg/onboarding/icon-3";
   import { BlurView } from "expo-blur";
-  import useUser, { setAuthorizationHeader } from "@/hooks/fetch/useUser";
+import  { setAuthorizationHeader, useFetchUser } from "@/hooks/fetch/fetchUserHook";
   import axios from "axios";
 const SupportCenter = () => {
   return (
