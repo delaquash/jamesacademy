b514f052dad70bd0caaaa7e4350b695baa8734bf
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-10 16:05:27 +0100

    feat: add SupportCenter component to support route

---
+++ app/(routes)/support/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import SupportCenter from '@/screen/SupportCenter/SupportCenter'
+
+const index = () => {
+  return (
+    <SupportCenter />
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
