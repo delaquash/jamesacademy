c56b17551d90569d6be555e52aa8d963559a1113
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-11 10:13:38 +0100

    feat: enhance SupportCenter component with new layout and functionality

--- screen/SupportCenter/SupportCenter.tsx
+++ screen/SupportCenter/SupportCenter.tsx
@@ -26,10 +26,200 @@ import {
 import  { setAuthorizationHeader, useFetchUser } from "@/hooks/fetch/fetchUserHook";
   import axios from "axios";
 const SupportCenter = () => {
+    const { theme } = useTheme()
+    const [open, setOpen] = useState(false)
+    const [ticketTitile, setTicketTitile] = useState("")
+    const [ticketDescription, setTicketDescription] = useState("")
+    const [loader, setLoader] = useState(false)
   return (
-    <View>
-      <Text>SupportCenter</Text>
-    </View>
+    <ScrollView
+        style={{
+            flex: 1,
+            backgroundColor: theme.dark ? "#101010" : "#fff",
+        }}
+        showsVerticalScrollIndicator={false}
+    >
+        {/* header options */}
+        <View
+            style={{
+                height: verticalScale(300),
+                overflow: "hidden",
+                position: "relative",
+                backgroundColor: theme.dark ? "#8673FC" : "#9DCDFF",
+                paddingTop: verticalScale(45),
+                paddingHorizontal: scale(20),
+            }}
+        >
+            <Pressable
+                style={{
+                    width: scale(35),
+                    height: scale(35),
+                    backgroundColor: "#fff",
+                    borderRadius: scale(5)
+                }}
+                onPress={()=> router.back()}
+            >
+                <AntDesign name="arrowleft" size={scale(22)} color="black" />
+            </Pressable>
+            <View style={{ flexDirection: "row"}}>
+                <SupportBannerOne />
+                <SupportBannerTwo />
+                <SupportBannerThree />
+            </View>
+        </View>
+
+        <View style={{ padding: scale(20) }}>
+            <Text
+                style={{
+                    color: theme.dark ? "#fff" : "#000",
+                    textAlign: "center",
+                    fontSize: fontSizes.FONT30,
+                    fontFamily: "Poppins_600SemiBold",
+                }}
+            >
+                 Tell us how can we help 👋
+            </Text>
+            <Text
+                style={{
+                    color: theme.dark ? "#fff" : "#000",
+                    textAlign: "center",
+                    fontSize: fontSizes.FONT18,
+                    fontFamily: "Poppins_400Regular",
+                }}
+            >
+                We are always available to provide you with the best services we can
+            </Text>
+            <Pressable
+                style={{
+                    height: !IsHaveNotch 
+                        ? verticalScale(65)
+                        : IsIPAD
+                        ? verticalScale(80)
+                        :verticalScale(62),
+                    backgroundColor: theme.dark ? "#3C43485c" : "#eaf3fb85",
+                    borderRadius: scale(10),
+                    shadowOpacity: 0.1,
+                    shadowColor: "#40E0D0",
+                    shadowRadius: 5,
+                    padding: scale(10),
+                    marginTop: verticalScale(20),
+                    flexDirection: "row",
+                    gap: scale(10),
+                }}
+                // onPress={()=>}
+            >
+                <IconOne />
+                <View>
+                    <Text
+                        style={{
+                            color: !theme.dark ? "#000" : "#fff",
+                            fontSize: fontSizes.FONT22,
+                            fontFamily: "Poppins_500Medium"
+                        }}
+                    >
+                        Chat
+                    </Text>
+                    <Text
+                        style={{
+                            color: !theme.dark ? "#000" : "#fff",
+                            fontSize: fontSizes.FONT20,
+                            fontFamily: "Poppins_400Regular",
+                            paddingTop: verticalScale(1),
+                        }}
+                     >
+                            Start a conversation now!
+                     </Text>
+                </View>
+            </Pressable>
+
+
+            <Pressable
+                style={{
+                    height: !IsHaveNotch
+                      ? verticalScale(65)
+                      : IsIPAD
+                      ? verticalScale(80)
+                      : verticalScale(62),
+                    backgroundColor: theme.dark ? "#3c43485c" : "#eaf3fb85",
+                    borderRadius: scale(10),
+                    shadowOpacity: 0.1,
+                    shadowColor: "#40E0D0",
+                    shadowRadius: 5,
+                    padding: scale(10),
+                    marginTop: verticalScale(20),
+                    flexDirection: "row",
+                    gap: scale(10),
+                }}
+                // onPress={() => router.push("/(routes)/faq")}
+            >
+                <IconTwo />
+                <View>
+            <Text
+              style={{
+                color: !theme.dark ? "#000" : "#fff",
+                fontSize: fontSizes.FONT22,
+                fontFamily: "Poppins_500Medium",
+              }}
+            >
+              FAQ's
+            </Text>
+            <Text
+              style={{
+                color: !theme.dark ? "#000" : "#fff",
+                fontSize: fontSizes.FONT20,
+                fontFamily: "Poppins_400Regular",
+                paddingTop: verticalScale(1),
+              }}
+            >
+              Find intelligent answers instantly
+            </Text>
+          </View>
+        </Pressable>
+
+        <Pressable
+          style={{
+            height: !IsHaveNotch
+              ? verticalScale(65)
+              : IsIPAD
+              ? verticalScale(80)
+              : verticalScale(62),
+            backgroundColor: theme.dark ? "#3c43485c" : "#eaf3fb85",
+            borderRadius: scale(10),
+            shadowOpacity: 0.1,
+            shadowColor: "#40E0D0",
+            shadowRadius: 5,
+            padding: scale(10),
+            marginTop: verticalScale(20),
+            flexDirection: "row",
+            gap: scale(10),
+          }}
+          onPress={() => Linking.openURL("mailto:support@becodemy.com")}
+        >
+          <IconThree />
+          <View>
+            <Text
+              style={{
+                color: !theme.dark ? "#000" : "#fff",
+                fontSize: fontSizes.FONT22,
+                fontFamily: "Poppins_500Medium",
+              }}
+            >
+              Email
+            </Text>
+            <Text
+              style={{
+                color: !theme.dark ? "#000" : "#fff",
+                fontSize: fontSizes.FONT20,
+                fontFamily: "Poppins_400Regular",
+                paddingTop: verticalScale(1),
+              }}
+            >
+              Get solution directly to inbox
+            </Text>
+          </View>
+        </Pressable>
+      </View>-
+    </ScrollView>
   )
 }
 
