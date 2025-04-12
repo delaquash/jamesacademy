7205aaced8416283115b7a66bfe9f5dbd101e222
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-04-12 19:19:52 +0100

    feat: enhance MyTicketScreen with additional imports and theme context

--- screen/MyTickets/MyTicketScreen.tsx
+++ screen/MyTickets/MyTicketScreen.tsx
@@ -1,7 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { useTheme } from "@/context/ThemeContext";
+import { scale, verticalScale } from "react-native-size-matters";
+import IconSix from "@/assets/svg/support-center/six";
+import { fontSizes } from "@/themes/app.constant";
+import { AntDesign } from "@expo/vector-icons";
+import { router } from "expo-router";
 
 const MyTicketScreen = () => {
+    const { theme } = useTheme();
   return (
     <View>
       <Text>MyTicketScreen</Text>
