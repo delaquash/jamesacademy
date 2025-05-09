bf731aa5930c3e996d8f6ad4d22140c59bffbee0
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-05-09 14:19:47 +0100

    feat: enhance CourseDetailScreen with dynamic content display and theming

--- screen/CourseDetail/CourseDetailScreen.tsx
+++ screen/CourseDetail/CourseDetailScreen.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
 import React, { useState } from 'react';
 import { useLocalSearchParams } from 'expo-router'
 import { useUserData } from '@/hooks/fetch/userData';
-
+import { useTheme } from '@/context/ThemeContext'
+import { fontSizes, IsAndroid, IsIPAD, SCREEN_WIDTH, windowHeight, windowWidth } from '@/themes/app.constant'
+import { verticalScale } from 'react-native-size-matters';
 const CourseDetailScreen = () => {
+    const { theme } = useTheme()
     const params: any = useLocalSearchParams()
     const [activeButtopn, setActiveButtopn] = useState("About")
     const {user, isLoading: userLoader } = useUserData()
@@ -14,8 +17,87 @@ const CourseDetailScreen = () => {
         params?.courseContent
       );
     return (
-        <View>
-        <Text>CourseDetailScreen</Text>
+        <View style={{ flex: 1, backgroundColor: theme.dark ? "#131313" : "#fff" }}>
+            <ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
+                <View style={{ padding: windowWidth(25)}}>
+                <Image
+            source={{
+              uri:
+                courseData.slug ===
+                "multi-vendor-mern-stack-e-commerce-project-with-all-functionalities-absolutely-for-beginners"
+                  ? "https://res.cloudinary.com/dwp4syk3r/image/upload/v1713574266/TMECA_yddc73.png"
+                  : courseData.slug ===
+                    "build-your-mobile-app-development-career-with-react-native"
+                  ? "https://res.cloudinary.com/dkg6jv4l0/image/upload/v1731448241/thumbnail_jwi5xo.png"
+                  : "https://res.cloudinary.com/dkg6jv4l0/image/upload/v1711468889/courses/spe7bcczfpjmtsdjzm6x.png",
+            }}
+            resizeMode="contain"
+            style={{
+              width: IsAndroid ? SCREEN_WIDTH - 40 : SCREEN_WIDTH - 25,
+              height: IsAndroid
+                ? (SCREEN_WIDTH - 28) * 0.5625
+                : (SCREEN_WIDTH - 40) * 0.5625,
+              alignSelf: "center",
+              borderRadius: windowWidth(10),
+            }}
+          />
+
+                    <Text
+                         style={{
+                            fontSize: fontSizes.FONT22,
+                            fontFamily: "Poppins_600SemiBold",
+                            paddingTop: verticalScale(10),
+                            color: theme.dark ? "#fff" : "#3E3B54",
+                            lineHeight: windowHeight(20),
+                          }}
+                    >
+                        {courseData.name}
+
+                    </Text>
+                    <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <Text
+                style={{
+                  fontSize: fontSizes.FONT22,
+                  fontFamily: "Poppins_400Regular",
+                  paddingTop: windowHeight(8),
+                  color: theme.dark ? "#fff" : "#000",
+                  lineHeight: windowHeight(20),
+                }}
+              >
+                ${courseData?.price}
+              </Text>
+              <Text
+                style={{
+                  fontSize: fontSizes.FONT22,
+                  fontFamily: "Poppins_400Regular",
+                  color: theme.dark ? "#fff" : "#3E3B54",
+                  lineHeight: IsIPAD ? windowHeight(0) : windowHeight(20),
+                  paddingLeft: windowWidth(5),
+                  textDecorationLine: "line-through",
+                }}
+              >
+                ${courseData?.estimatedPrice}
+              </Text>
+            </View>
+            <Text
+              style={{
+                fontSize: fontSizes.FONT18,
+                fontFamily: "Poppins_400Regular",
+                color: theme.dark ? "#fff" : "#000",
+              }}
+            >
+              {courseData?.purchased} Students
+            </Text>
+          </View>
+                </View>
+            </ScrollView>
         </View>
     )
 }
