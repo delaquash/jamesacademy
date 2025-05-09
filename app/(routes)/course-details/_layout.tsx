986fdf23a036a55e72e0bc5cede230fc43a6fc16
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-05-09 17:57:33 +0100

    feat: add fetchCourse hook with TypeScript support and error handling

---
+++ app/(routes)/course-details/_layout.tsx
@@ -0,0 +1,17 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { router, Slot, Stack } from "expo-router";
+import { useTheme } from "@/context/ThemeContext";
+import { fontSizes } from "@/themes/app.constant";
+import { AntDesign } from "@expo/vector-icons";
+import { scale } from "react-native-size-matters";
+
+export default function _layout() {
+  const { theme } = useTheme();
+
+  return (
+    <Stack>
+        
+    </Stack>
+  )
+}
--- hooks/fetch/fetchCourse.tsx
+++ hooks/fetch/fetchCourse.tsx
@@ -1,17 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryFunctionContext, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchCourse = async ({ queryKey }) => {
+// Define query key type
+type CourseQueryKey = [string, { isProfile: boolean }];
+
+const fetchCourse = async ({ queryKey }: QueryFunctionContext<CourseQueryKey>) => {
   const [_key, { isProfile }] = queryKey;
   
   // Using a single endpoint but passing isProfile as a query parameter
-  const endpoint = `${process.env.EXPO_PUBLIC_SERVER_URI}/course/get-courses`;
-  
-  const res = await axios.get(endpoint, { 
-    params: { isProfile } 
-  });
-  
-  return res.data.courses;
+  try {
+      // Using a single endpoint but passing isProfile as a query parameter
+    const res = await axios.get(`${process.env.EXPO_PUBLIC_SERVER_URI}/course/get-courses`, { params: { isProfile } });
+    return res.data.courses;
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    throw error;
+  }
 };
   
 export const fetchCourseHook = (isProfile = false) => {
@@ -19,6 +23,9 @@ export const fetchCourseHook = (isProfile = false) => {
     queryKey: ["course", { isProfile }],
     queryFn: fetchCourse,
     retry: 3,
+    staleTime: 5 * 60 * 1000, // 5 minutes
+    // cacheTime: 30 * 60 * 1000, // 30 minutes
+    refetchOnWindowFocus: false,
   });
 
   return {
